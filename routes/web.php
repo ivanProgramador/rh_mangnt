819e9ab28c79c597a9387fd52aebeeb1e1ff5292
ivanProgramador <ivan-guita@hotmail.com>
2025-09-03 21:58:37 -0300

    sistema de email testado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
+
+
 
 Route::get('/', function () {
    echo'Rh mangnt';
+});
+
+Route::get('/email', function () {
+
+    Mail::raw('Mensagem de teste rh mangnt',function(Message $message){
+       $message->to('teste@gmail.com')
+       ->subject('Bem vindo ao Rh mangnt')
+       ->from('rh@rhmangnt.com');
+    });
+
+
+    echo'email enviado com sucesso';
 });
