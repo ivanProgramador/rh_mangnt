5c284cc5d4ac1f5ad1fc6ec92626c16065851305
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-09-04 21:54:27 -0300

    corrigir bug da view admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Mail\Message;
 use Illuminate\Support\Facades\Mail;
+use App\Models\User;
+
 
 
 
@@ -20,4 +22,11 @@ Route::get('/email', function () {
 
 
     echo'email enviado com sucesso';
+});
+
+Route::get('/admin',function(){
+
+   $admin = User::with('detail','department')->find(1);
+   return view('admin',compact($admin));
+
 });
