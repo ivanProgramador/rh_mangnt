6d3a07bedee3748338e710ce4b65d842f2270cdc
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-09-21 12:05:00 -0300

    view e classe do email configuradas

---
+++ app/Http/Controllers/ConfirmAccountController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ConfirmAccountController extends Controller
+{
+    public function confirmAccount($url){
+
+        echo 'Estou aqui '.$url;
+
+    }
+}
---
+++ resources/views/mail/confirm-account-email.blade.php
@@ -0,0 +1,7 @@
+<p>Bem vindo a confirmação de registro do sistema {{ env('APP_NAME')  }} ! </p><br>
+<hr>
+<P>Para concluir o seu cadastro clique no link abaixo para definir a sua senha </P><br>
+
+<p><a href="{{ $url }}"> Clique aqui para definir sua senha </a></p><br>
+
+<p>Att, gestão do sistema {{ env('APP_NAME')  }} </p>
